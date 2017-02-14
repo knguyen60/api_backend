5da355c47081532a093e2b41f30bbf3da81541d6
EC2 Default User <ec2-user@ip-172-31-54-224.ec2.internal>
2017-02-14 23:27:53 +0000

    static

--- api_backend/settings.py
+++ api_backend/settings.py
@@ -143,7 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = "/home/ec2-user/api_backend/api_backend/static/"
 
 
 REST_FRAMEWORK = {
